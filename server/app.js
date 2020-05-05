8bf3b33b5b606d934af9ada1a954dfe125369e12
oneshotaxe <oneshotaxe@ya.ru>
2020-05-05 11:04:00 +0300

    db reset

--- server/app.js
+++ server/app.js
@@ -3,36 +3,58 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
 
-// var fs = require('fs');
-// var Bus = require('./models').Bus
-// var Driver = require('./models').Driver
+var fs = require('fs');
+var Bus = require('./models').Bus
+var Driver = require('./models').Driver
+var Route = require('./models').Route
+var Way = require('./models').Way
 
 const app = express()
 mongoose.set('useFindAndModify', false);
 mongoose.connect('mongodb://localhost/base', {useNewUrlParser: true, useUnifiedTopology: true}, function (err) {
    if (err) throw err;
-   // fs.readFile('server/db_buses.txt', 'utf8', function(err, contents) {
-   //    contents.split('\n').forEach((value) => {
-   //       let data = value.split(',')
-   //       let document = { busnumber: data[0], mark: data[1], color: data[2], capacity: data[3] }
-   //       let model = new Bus(document)
-   //       model.save()
-   //    })
-   // })
-   // fs.readFile('server/db_drivers.txt', 'utf8', function(err, contents) {
-   //    contents.split('\n').forEach((value) => {
-   //       let data = value.split(';')
-
-   //       let oldGraphic = JSON.parse(data[3])
-   //       let newGraphic = { date: oldGraphic.startDate.substr(0,10), items: oldGraphic.graphicItems.map((value) => ({ status: value.Status, days: ''+value.NumberOfDays }))}
-         
-   //       Bus.findOne({busnumber: data[2]}, (err, res) => {
-   //          let document = { tabnumber: data[0], name: data[1], bus: res._id, graphic: newGraphic }
-   //          let model = new Driver(document)
-   //          model.save()
-   //       })
-   //    })
-   // })
+   fs.readFile('server/db.csv', 'utf8', async function(err, contents) {
+      await Bus.deleteMany()
+      await Driver.deleteMany()
+      await Route.deleteMany()
+      await Way.deleteMany()
+      contents.split('\r\n').forEach(async (value) => {
+         let data = value.split(';')
+         let route = null,
+            way = null,
+            bus = null,
+            driver = null
+         if (data[0]) {
+            route = await Route.findOne({ title: data[0] })
+            if (!route) {
+               route = new Route({ title: data[0] })
+               await route.save()
+            }
+            way = await Way.findOne({ title: data[1] })
+            if (!way) {
+               way = new Way({ title: data[0], route: route._id })
+               await way.save()
+            }
+         }
+         if (data[2]) {
+            bus = await Bus.findOne({ busnumber: data[2] })
+            if (!bus) {
+               bus = new Bus({ busnumber: data[2] })
+               if (way) {
+                  bus.way = way._id
+               }
+               await bus.save()
+            }
+         }
+         if (data[5]) {
+            driver = await Driver.findOne({ tabnumber: data[5] })
+            if (!driver) {
+               driver = new Driver({ tabnumber: data[5], name: data[3], bus: bus._id })
+               await driver.save()
+            }
+         }
+      })
+   })
 });
 
 app.use(cookieParser())
