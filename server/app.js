cceb4c8daa33bba63aab0a8ab3a5937e9e196d00
oneshotaxe <oneshotaxe@ya.ru>
2020-05-05 12:17:34 +0300

    'm'

--- server/app.js
+++ server/app.js
@@ -13,51 +13,51 @@ const app = express()
 mongoose.set('useFindAndModify', false);
 mongoose.connect('mongodb://localhost/base', { useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
    if (err) throw err;
-   fs.readFile('server/db.csv', 'utf8', async function (err, contents) {
-      console.log('log:', err)
-      await Bus.deleteMany()
-      await Driver.deleteMany()
-      await Route.deleteMany()
-      await Way.deleteMany()
-      const rows = contents.split('\n')
-      for (let i = 0; i < rows.length; i++) {
-         const value = rows[i]
-         let data = value.split(';')
-         let route = null,
-            way = null,
-            bus = null,
-            driver = null
-         if (data[0]) {
-            route = await Route.findOne({ title: data[0] })
-            if (!route) {
-               route = new Route({ title: data[0] })
-               await route.save()
-            }
-            way = await Way.findOne({ title: data[1], route: route._id })
-            if (!way) {
-               way = new Way({ title: data[1], route: route._id })
-               await way.save()
-            }
-         }
-         if (data[2]) {
-            bus = await Bus.findOne({ busnumber: data[2] })
-            if (!bus) {
-               bus = new Bus({ busnumber: data[2].toString() })
-               if (way) {
-                  bus.way = way._id
-               }
-               await bus.save()
-            }
-         }
-         if (data[5]) {
-            driver = await Driver.findOne({ tabnumber: data[5] })
-            if (!driver) {
-               driver = new Driver({ tabnumber: data[5], name: data[3], bus: bus._id })
-               await driver.save()
-            }
-         }
-      }
-})
+//    fs.readFile('server/db.csv', 'utf8', async function (err, contents) {
+//       console.log('log:', err)
+//       await Bus.deleteMany()
+//       await Driver.deleteMany()
+//       await Route.deleteMany()
+//       await Way.deleteMany()
+//       const rows = contents.split('\n')
+//       for (let i = 0; i < rows.length; i++) {
+//          const value = rows[i]
+//          let data = value.split(';')
+//          let route = null,
+//             way = null,
+//             bus = null,
+//             driver = null
+//          if (data[0]) {
+//             route = await Route.findOne({ title: data[0] })
+//             if (!route) {
+//                route = new Route({ title: data[0] })
+//                await route.save()
+//             }
+//             way = await Way.findOne({ title: data[1], route: route._id })
+//             if (!way) {
+//                way = new Way({ title: data[1], route: route._id })
+//                await way.save()
+//             }
+//          }
+//          if (data[2]) {
+//             bus = await Bus.findOne({ busnumber: data[2] })
+//             if (!bus) {
+//                bus = new Bus({ busnumber: data[2].toString() })
+//                if (way) {
+//                   bus.way = way._id
+//                }
+//                await bus.save()
+//             }
+//          }
+//          if (data[5]) {
+//             driver = await Driver.findOne({ tabnumber: data[5] })
+//             if (!driver) {
+//                driver = new Driver({ tabnumber: data[5], name: data[3], bus: bus._id })
+//                await driver.save()
+//             }
+//          }
+//       }
+// })
 });
 
 app.use(cookieParser())
